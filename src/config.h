335b8c396bd93a72d74a240463267b853c6dfb75
Karl Lenz <xorangekiller@gmail.com>
2014-02-26 23:54:41 -0500

    Implemented a new interprocess communication handler
    The pipe communication mechanism introduced in this commit is a C rewrite
    based on my former C++ code, but, more importantly, it also represents a major
    improvement in robustness, code quality, and functionality. The internal
    protocol implemented in this rewrite IS NOT COMPATIBLE with the old
    (SimplePost 1.2.x) implementation. If either one tries to interact with the
    other, they will both choke and die a painful death. I have no intention of
    fixing this. This is a breaking change, but it hopefully lays the groundwork
    for a more stable pipe command infrastructure for more compatability between
    future releases.
    
    The implementation in this commit is not quite complete. It compiles, but it
    has not been tested because it is missing an implementation of some of the
    pipe commands it is supposed to respond to. The internal SimplePost web server
    needs to be reimplemented in C before this pipe infrastructure can be
    completed.

---
+++ src/config.h
@@ -0,0 +1,33 @@
+/*
+SimplePost - A Simple HTTP Server
+
+Copyright (C) 2012-2014 Karl Lenz.  All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have recieved a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 021110-1307, USA.
+*/
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/*
+Global configuration details
+*/
+#define SP_MAIN_SHORT_NAME  "simplepost"                            // Short program name
+#define SP_MAIN_DESCRIPTION "SimplePost"                            // Description of the program
+#define SP_MAIN_VERSION     "0.2.1"                                 // Program version
+#define SP_MAIN_COPYRIGHT   "Copyright (C) 2012-2014 Karl Lenz."    // Program copyright
+
+#endif // _CONFIG_H_
